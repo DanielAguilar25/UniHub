bc7dafdb128bf6f5747115c3a222b6fb72ea5f0f
Daniel Aguilar <daniel.aguilar06@utrgv.edu>
2026-03-28 17:19:42 -0500

    added cta band and footer

---
+++ src/components/home/CTABand.jsx
@@ -0,0 +1,13 @@
+import '../../../src/styles/CTABand.css';
+
+export default function CTABand() {
+  return (
+    <div className="uh-cta-band">
+      <div>
+        <h2>Ready to connect?</h2>
+        <p>Join hundreds of CECS students already using UniHub.</p>
+      </div>
+      <button className="uh-btn-large">Create your profile</button>
+    </div>
+  );
+}
